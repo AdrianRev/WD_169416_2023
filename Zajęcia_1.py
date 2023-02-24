6e31f9a51aa9f40a3a1346cfb267791a5ef0a2fc
AdrianRev <115699706+AdrianRev@users.noreply.github.com>
2023-02-24 12:03:21 +0100

    Add files via upload

---
+++ Zajęcia_1.py
@@ -0,0 +1,32 @@
+for i in range(1, 5):
+    print("******************")
+print("")
+
+print("******************")
+print("*                *")
+print("*                *")
+print("******************\n")
+
+
+gwiazdka:str = "*"
+for i in range(1, 5)
+    print(gwiazdka*i)
+
+
+a = (512-282)/(47*48+5)
+print(a)
+
+x = 7
+for i in range(1, 5):
+    print(x*i, end="---")
+print(x*5)
+
+Lorem ="Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym. Spopularyzował się w latach 60. XX w. wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker"
+
+a = eval(input("Wpisz liczbę:"))
+b = eval(input("Wpisz drugą liczbę:"))
+print(a*b)
+
+c = eval(input("Podaj wagę w kilogramach:"))
+d = (c*2.2)
+print("{} kilogramów jest równe {} funtów".format(c, d))
